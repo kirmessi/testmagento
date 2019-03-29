40fc4d02a7b580f7f5dcdd05fdc7135a43eb1e62
kirmessi <kirmessi2394@gmail.com>
2019-03-29 11:47:03 +0200

    finished 7 task with knockout js library

--- app/code/Kir/Module/view/frontend/web/js/library.js
+++ app/code/Kir/Module/view/frontend/web/js/library.js
@@ -6,17 +6,20 @@ define([
     'use strict';
 
     return Component.extend({
+
         initialize: function () {
             this._super();
+            this.name = ko.observable('');
+            this.getName = ko.observable();
+            this.font = ko.observable(-1000);
             return this;
         },
         onSubmit: function() {
+            var data = {'name':this.name()};
+            this.getName(data.name);
+            this.font(1000);
 
-                console.log(('#knoutname').val());
-
-        },
-        getText: function () {
-            return "call the function here..";
         }
+
     });
 });
